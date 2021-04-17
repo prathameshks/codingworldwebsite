a2160aa90c1de6991c35d5010a70d82dee4abe50
prathameshks <82529994+prathameshks@users.noreply.github.com>
2021-04-17 15:12:59 +0530

    Added main files

---
+++ projects.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title></title>
+    <meta name="description" content="Official website of Code World.Get All video code information programs and Projects.">
+    <meta name="author" content="Code World">
+    <link rel="stylesheet" href="css/style.css">
+
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <style>
+        #projects {
+            text-shadow: 0 0 2px #fff;
+            background-color: #bababa;
+            padding: 5px 15px;
+            border-radius: 5px;
+        }
+    </style>
+</head>
+
+<body>
+    <?php include('header.html'); ?>
+    <br>
+
+    <br>
+    <?php include('footer.html'); ?>
+</body>
+<html>
